6109b17b48a8ddcbd40b2aa3cc26ce71d7ac6ca6
Lizeth Mayta.M <echenique123lizeth@gmail.com>
2026-03-16 10:36:38 -0400

    programa verificar numero par

---
+++ Laboratorio-proyecto/programa_par.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class programa_par {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int numero = sc.nextInt();
+
+        // Lógica para saber si es par
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es par.");
+        } else {
+            System.out.println(numero + " es impar.");
+        }
+
+        sc.close();
+    }
+}
